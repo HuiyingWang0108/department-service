31be6dfa787f5ec6da4afcfd6d64728dc882fbfb
Huiying Wang <whymissu1228@gmail.com>
2022-11-08 18:15:22 -0800

    fixed YAML file cannot be loaded

--- src/main/java/com/dailycode/departmentservice/DepartmentServiceApplication.java
+++ src/main/java/com/dailycode/departmentservice/DepartmentServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application");
 		SpringApplication.run(DepartmentServiceApplication.class, args);
 	}
 
